bdc511e20923c88a3e6a7319b957c37e57c4d6e6
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-28 02:24:52 +0100

    [Add] Parser Class created (ParkingEscape)

---
+++ ParkingEspace/src/Parser.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Abde / Cedric
+ */
+public class Parser {
+    /* créer chaque méthode
+       pour parser X ou Y information, par exemple
+    
+        Car goal = new Parser.getGoal()
+        Car[] cars = new Parser.getCars()
+        int dimension = Parser.getDimension()
+    
+        etc~
+    */
+}
