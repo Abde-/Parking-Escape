d8cd1b62600df30b728a8f921b0df84b0ca3eea2
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-28 03:48:50 +0100

    [MàJ] Some constructors / methods in Car and Node classes.

--- ParkingEspace/src/Car.java
+++ ParkingEspace/src/Car.java
@@ -5,4 +5,19 @@
 
 public class Car {
     
+    private boolean orient;
+    private boolean goal;
+    
+    public Car(int[] coord1,int[] coord2, boolean x){
+        // pas de gestion des coordonnées, on suppose que c'est juste
+        if (coord1[0] == coord2[0]) orient = true;
+        if (coord1[1] == coord2[1]) orient = false;
+        
+        goal = x;
+    } 
+
+    // créer methodes de mouvement etc
+    
+    
+    
 }
--- ParkingEspace/src/Node.java
+++ ParkingEspace/src/Node.java
@@ -4,4 +4,12 @@
  */
 public class Node {
     
+    private int weight; // poids chemin
+    private Car[] cars;
+    private Car goal;
+    
+    public Node( /* mettre en parametre weight+1 voire voitures */ ){
+        
+    }
+    
 }
