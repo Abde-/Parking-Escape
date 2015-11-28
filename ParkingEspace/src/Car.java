c64e3d55e6c3630c3e5b383d928ad1abcf89cf5b
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-28 16:46:16 +0100

    final private attributes in car

--- ParkingEspace/src/Car.java
+++ ParkingEspace/src/Car.java
@@ -5,13 +5,18 @@
 
 public class Car {
     
-    private boolean orient;
-    private boolean goal;
+    // variables 'final' indiquent que ces valeurs sont seulement assignées une
+    // seule fois, ça ne sert pas à grande chose mais pour info c'est cool de savoir
+    // que ces statuts ne changent pas (les voitures ne changent pas d'orientation
+    // et la voiture goal reste goal
+    
+    private final boolean orient;
+    private final boolean goal;
     
     public Car(int[] coord1,int[] coord2, boolean x){
         // pas de gestion des coordonnées, on suppose que c'est juste
         if (coord1[0] == coord2[0]) orient = true;
-        if (coord1[1] == coord2[1]) orient = false;
+        else if (coord1[1] == coord2[1]) orient = false;
         
         goal = x;
     } 
