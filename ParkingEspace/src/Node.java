ab55ae938517a0b307bed4183c030c4b7673c859
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-29 04:18:27 +0100

    Comment in Node class

--- ParkingEspace/src/Node.java
+++ ParkingEspace/src/Node.java
@@ -8,8 +8,8 @@ import java.util.HashMap; // Pour vérifier que nouveau node dans Hashtamèyr
 public class Node {
     
     private int weight; // poids chemin
-    private Car[] cars;
-    private Car goal;
+    private Car[] cars; 
+    private Car goal; // <- ou cars[0] = goal ?
     
     public Node( /* mettre en parametre weight+1 voire voitures */ ){
         
