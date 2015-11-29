66a036c034cb898fe91f68b0d4dc21ac1caed996
Abdeselam El-Haman <aelhaman@gmail.com>
2015-11-29 01:55:41 +0100

    [Add] move method in Car class

--- ParkingEspace/src/Car.java
+++ ParkingEspace/src/Car.java
@@ -3,8 +3,6 @@
  * @author Abdeselam / Cedric
  */
 
-import java.lang.Math;
-
 public class Car {
     
     // variables 'final' indiquent que ces valeurs sont seulement assignées une
@@ -14,13 +12,19 @@ public class Car {
     
     private final boolean vertical;
     private final boolean goal;
+    
+    // -- on suppose que front est la partie la plus proche de la fin (le haut pour le vertical
     private int[] front;
     private int[] behind;
+    // -- et la droite pour l'horizontal)
     
-    public Car(int[] coord1,int[] coord2, boolean x, int[] exit){
+    private final int[] exit;
+    
+    public Car(int[] coord1,int[] coord2, boolean x, int[] ex){
         // pas de gestion des coordonnées, on suppose que c'est juste
         vertical = coord1[1] == coord2[1];
-        goal = x; 
+        goal = x;
+        exit = ex;
         
         if(goal){
             int index = 0;
@@ -28,40 +32,63 @@ public class Car {
             if (vertical){
                 index = 1;
             }
-            if (Math.abs(coord1[index]-exit[index]) < Math.abs(coord2[index]-exit[index])){
+            if ( coord1[index] > coord2[index] ){
                 front = coord1; behind = coord2;
             }
             else{
                 behind = coord1; front = coord2;
             }
         }
-        
     }
     
     public int move(boolean x, int[] dim){
         // boolean x indique vers quelle direction la voiture va
-        int weight;
+        int weight = 0;
         // if x -> voiture avance vers le front, sinon vers l'arrière
         // pour la voiture goal, avancer vers le front = vers l'entrée
-        
-        
+
         
         if(x){
-            if(){
-                // à finir chez moi
+            // bouger vers le front
+            if(vertical){
+                if (front[1]+1 < dim[1] ){ front[1] = front[1] +1; behind[1] = behind[1] +1;
+                    weight = -1;}
             }
-            
+            else{
+                if (front[0]+1 < dim[0] ){ front[0] = front[0] +1; behind[0] = behind[0] +1; 
+                    weight = -1;}
+            }
+            // -- à modifier -> par rapport à la sortie
+            if (goal && weight != -1){ weight = 0;}
+            else if ( weight != -1 ){ weight = 1; }
+            // -- à modifier
         }
         else{
-            
+            // bouger vers l'arriere
+            if(vertical){
+                if (behind[1]-1 >= 0 ){ front[1] = front[1] -1; behind[1] = behind[1] -1;
+                    weight = -1;}
+            }
+            else{
+                if (behind[0]-1 >= 0 ){ front[0] = front[0] -1; behind[0] = behind[0] -1; 
+                    weight = -1;}
+            }
+            // -- à modifier -> par rapport à la sortie
+            if (goal && weight != -1){ weight = 2;}
+            else if ( weight != -1 ){ weight = 1; }
+            // -- à modifier
         }
+        
         // return poids du mouvement, si weight == -1, mouvement impossible
-        // voiture sort de
+        // voiture sort du parking
         return weight;
     }
-
+    
+    public int[] getFront(){
+        return front;
+    }
+    public int[] getBehind(){
+        return behind;
+    }
     // créer methodes de mouvement etc
-    
-    
-    
 }
