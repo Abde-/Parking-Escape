333bf20048efd601c86127f39533f0e507eb415b
Abdeselam El-Haman Abdeselam <aelhaman@gmail.com>
2015-12-04 20:40:57 +0100

    [Node] Contructeur, contructeur de copie dans voiture etc...

--- ParkingEspace/src/Node.java
+++ ParkingEspace/src/Node.java
@@ -8,11 +8,45 @@ import java.util.HashMap; // Pour vérifier que nouveau node dans Hashtamèyr
 public class Node {
     
     private int weight; // poids chemin
-    private Car[] cars; 
-    private Car goal; // <- ou cars[0] = goal ?
+    private int[] dim;
+    private Car[] cars;
+    private Car goal;
+    private int[] exit;
+    QueueManager queue;
     
-    public Node( /* mettre en parametre weight+1 voire voitures */ ){
+    public Node(Car[] voitures, Car paramgoal, int poids, int[] dimension, int[] ex, QueueManager q) {
+        cars = new Car[voitures.length];
+        for(int i = 0; i < voitures.length; ++i){
+            cars[i] = new Car(voitures[i]);
+        }
+        goal = new Car(paramgoal);
+        dim = dimension;
+        exit = ex;
+        queue = q;
+    }
+    
+    public void extend(){
+        // création de nouveaux noeuds et rajouter à queue
+        int newWeight = goal.move(dim, cars, true);
+        if (newWeight != -1) queue.addToQueue(new Node(cars,goal,weight+newWeight,dim,exit,queue));
+        newWeight = goal.move(dim,cars,false);
+        if (newWeight != -1) queue.addToQueue(new Node(cars,goal,weight+newWeight,dim,exit,queue));
+        
+        Car[] tempCarlist = new Car[cars.length-1];
+        
         
     }
     
+    @Override 
+    public int hashCode(){
+        // fonction de hashage par rapport aux coordonnées des voitures
+    }
+    
+    public int getWeight(){
+        return weight;
+    }
+    
+    public boolean isResult(){
+        return goal.isInDomain(exit);
+    }
 }
