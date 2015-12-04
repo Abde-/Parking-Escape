c33be25cfd4afb58324238a22cec3d65a0bd61e0
Abdeselam El-Haman Abdeselam <aelhaman@gmail.com>
2015-12-04 18:06:07 +0100

    [Fix/Test] Classe voiture testée et fonctionnelle.
    Pas testée en vertical, mais ça devrait marcher

--- ParkingEspace/src/Car.java
+++ ParkingEspace/src/Car.java
@@ -7,10 +7,10 @@ import java.util.Iterator;
 
 public class Car {
     
-    private Stack<int[][]> steps;
+    private Stack<int[][]> steps = new Stack<>();
     private final boolean goal;
     
-    private final int orient; // la direction vers laquelle bouge la voiture
+    private final int orient; // la direction vers laquelle bouge la voiture (vertical ou horizontal)
     private final boolean exitorient; // la direction vers laquelle est l'exit
     
     private int[] front = new int[2];
@@ -30,14 +30,14 @@ public class Car {
             behind[0] = coord1[0]; behind[1] = coord1[1];
         }
         
-        exitorient = Math.abs(front[orient] - exit[orient]) < Math.abs(behind[orient] - exit[orient]);
+        exitorient = Math.abs(front[orient] - exit[orient]) < Math.abs(behind[orient] - exit[orient]) ;
         
         // on rajoute l'étape -> situation initiale -> new int car copie des valeurs, si on rajoutait
         // directenemt new int[][] -> on copie la référence de l'objet du coup on aura une liste d'éléments identiques
-        steps.add(new int[][] { new int[] {behind[0],behind[1]}, new int[] {front[0],front[1]}});
+        steps.add(new int[][] { {behind[0],behind[1]},{front[0],front[1]}});
     }
     
-    ////////////////////////////////////// à tester
+
     // oui Cedric, finalement on passe la liste de Cars et on fait le mouvement
     // par rapport à ce check
     public int moveFront(int[] dim, Car[] cars, boolean direction){
@@ -47,11 +47,11 @@ public class Car {
         behind[orient] = behind[orient] +movement; front[orient] = front[orient] +movement;
         
         // attribution de poids de mouvement
-        int weight = goal ? 0 : 1;
-        if (weight == 2 && exitorient == direction) weight = 2; // si la voiture goal bouge vers l'exit;
+        int weight = goal ? 2 : 1;
+        if (weight == 2 && exitorient == direction) weight = 0; // si la voiture goal bouge vers l'exit;
         
         // vérification que mouvement possible
-        if (behind[orient] >= 0 || front[orient] < dim[orient]){
+        if (behind[orient] >= 0 && front[orient] < dim[orient]){
             int i = 0;
             while ( i < cars.length && weight != -1 ){
                 if(this.isIntersect(cars[i])){
@@ -63,16 +63,16 @@ public class Car {
         else weight = -1;
         
         // si mouvement valide on rajoute le mouvement dans stack de mouvements, sinon
-        // on reprends les dernières coordonnées possibles
+        // on reprend les dernières coordonnées possibles
         if (weight == -1){
             behind[0] = steps.lastElement()[0][0]; behind[1] = steps.lastElement()[0][1];
             front[0] = steps.lastElement()[1][0]; front[1] = steps.lastElement()[1][1];
         }
-        else steps.add(new int[][] { new int[] {behind[0],behind[1]}, new int[] {front[0],front[1]}});
+        else steps.add(new int[][] { {behind[0],behind[1]},{front[0],front[1]}});
 
         return weight;
     }
-    ////////////////////////////////////// à tester
+
     
     public boolean isIntersect(Car other){
         boolean res = false;
@@ -84,13 +84,14 @@ public class Car {
 
     public boolean isInDomain(int[] coord){
         boolean res = false;
-        if ( (coord[0] == front[0] && coord[1] == front[1]) &&
+        if ( (coord[0] == front[0] && coord[1] == front[1]) ||
                 (coord[0] == behind[0] && coord[1] == behind[1]) ){
             res = true;
         }
         return res;
     }
     
+    // afficher etapes
     public void printSteps(String carName){
         Iterator<int[][]> stepIterator = steps.iterator();
         int[][] current = stepIterator.next();
--- ParkingEspace/src/ParkingEspace.java
+++ ParkingEspace/src/ParkingEspace.java
@@ -11,12 +11,16 @@ public class ParkingEspace {
      * @param args le path du fichier test (pour NetBeans on teste directement dans la ligne 16)
      * @throws FileNotFoundException 
      */
-    
+        
     public static void main(String[] args) throws FileNotFoundException{
         Parser x = new Parser("test/test.txt");
         
         System.out.println("Exit: " + Integer.toString(x.getExit()[0])+" "+Integer.toString(x.getExit()[1]));
         
-        
+        Car goalCar = x.getGoal();
+        for(int i = 0; i < 5; ++i){
+            goalCar.printSteps("Goal");
+            System.out.println(goalCar.moveFront(x.getDimension(), new Car[] {x.getCars()[0]}, true));
+        }
     }
 }
